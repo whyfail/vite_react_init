8c721075a3cd4af4efc9c6d7179e0ac20891fb95
吴磊 <wulei@santu.com>
2022-11-24 15:23:27 +0800

    [功能]：增加代码打包拆分插件vite-plugin-chunk-split

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import viteCompression from 'vite-plugin-compression';
 import progress from 'vite-plugin-progress';
 import postCssPxToRem from 'postcss-pxtorem';
 import vitePluginNoBug from 'vite-plugin-no-bug';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,6 +25,7 @@ export default defineConfig({
       targets: ['defaults', 'not IE 11'],
     }),
     vitePluginNoBug(),
+    chunkSplitPlugin(),
   ],
   css: {
     postcss: {
