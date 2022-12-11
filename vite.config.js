6c4bf38e94127a697433a81218fee84b389f1c6e
The Bug <xxxx@xxx.xxx>
2022-12-11 12:19:00 +0800

    [升级]：升级vite等库版本，以及更换react编译插件未swc插件

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import legacy from '@vitejs/plugin-legacy';
 import { visualizer } from 'rollup-plugin-visualizer';
 import viteCompression from 'vite-plugin-compression';
