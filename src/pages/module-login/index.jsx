af161c3b982af449169dc4502c6e5f26f37d4c82
吴磊 <wulei0407@163.com>
2023-07-24 09:45:32 +0800

    [功能]:添加clsx库,用于添加多个className用

--- src/pages/module-login/index.jsx
+++ src/pages/module-login/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'antd';
 import { KEY_TOKEN } from '@/apis';
+import clsx from 'clsx';
 
 const LoginIndex = () => {
   const navigate = useNavigate();
 
   return (
-    <center style={{ paddingTop: '20%' }}>
+    <center style={{ paddingTop: '20%' }} className={clsx('login_index', 'center')}>
       <Button onClick={() => navigate('/')}>跳转主页</Button>
       <Button
         onClick={() => {
