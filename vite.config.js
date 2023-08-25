b5b93165bca2254623edf5452b13934292e003fe
吴磊 <wulei0407@163.com>
2023-08-25 16:43:53 +0800

    [优化]:优化打包速度与碎片化

--- vite.config.js
+++ vite.config.js
@@ -60,8 +60,10 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 1500,
+    reportCompressedSize: false,
     rollupOptions: {
       output: {
+        experimentalMinChunkSize: 1000,
         manualChunks: {
           react: ['react', 'react-router-dom'],
           lodashEs: ['lodash-es'],
