5d042b9e0008862bfcfb2c678e32b5b7a4e286cc
吴磊 <wulei0407@163.com>
2025-12-19 10:06:34 +0800

    refactor: 删除未使用的storeSystem模块

--- src/stores/storeSystem.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-const useStoreSystem = create(set => ({
-  systemMenuFull: true,
-  setSystemMenuFull: val => set(() => ({ systemMenuFull: val })),
-}));
-
-export default useStoreSystem;
