8f6869892e427bf5d40e822a800a27d9d804e19f
吴磊 <wulei0407@163.com>
2025-12-24 16:30:44 +0800

    refactor(build): 替换 vite 压缩和打包分析插件
    用 vite-bundle-analyzer 和 vite-plugin-compression2 替换 rollup-plugin-visualizer 和 vite-plugin-compression
    优化构建配置，支持 brotli 压缩算法并集成打包分析功能

--- vite.config.js
+++ vite.config.js
@@ -4,111 +4,110 @@ import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 import { codeInspectorPlugin } from 'code-inspector-plugin';
 import million from 'million/compiler';
-import { visualizer } from 'rollup-plugin-visualizer';
 import UnoCSS from 'unocss/vite';
 import Printer from 'unplugin-printer/vite';
 import { defineConfig } from 'vite';
-import viteCompression from 'vite-plugin-compression';
+import { analyzer, unstableRolldownAdapter } from 'vite-bundle-analyzer';
+import { compression } from 'vite-plugin-compression2';
 import vitePluginNoBug from 'vite-plugin-no-bug';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  return {
-    base: './',
-    plugins: [
-      million.vite({
-        auto: {
-          threshold: 0.05,
+export default defineConfig(() => ({
+  base: './',
+  plugins: [
+    million.vite({
+      auto: {
+        threshold: 0.05,
+      },
+    }),
+    codeInspectorPlugin({
+      bundler: 'vite',
+    }),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    unstableRolldownAdapter(analyzer({
+      openAnalyzer: true, // 是否构建完后打开分析器
+    })),
+    compression({
+      algorithms: ['gzip', 'brotliCompress'], // 压缩算法 nginx需增相应配置
+    },
+    ),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    UnoCSS({
+      configFile: './unocss.config.js',
+    }),
+    vitePluginNoBug(),
+    Printer({
+      info: [
+        ({ lightCyan, green, bold }) => {
+          return `  ${green('➜')}  ${bold('官网')}:  ${lightCyan('https://whyfail.github.io/cwa-docs')}`;
         },
-      }),
-      codeInspectorPlugin({
-        bundler: 'vite',
-      }),
-      react({
-        babel: {
-          plugins: ['babel-plugin-react-compiler'],
+      ],
+    }),
+    webUpdateNotice({
+      notificationProps: {
+        title: '系统升级通知',
+        description: '检测到当前系统版本已更新，请刷新页面后使用',
+        buttonText: '刷新',
+        dismissButtonText: '忽略',
+      },
+    }),
+  ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ['> 1%'],
+        }),
+      ].filter(Boolean),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    host: true,
+    open: true,
+    proxy: {
+      // 代理
+      '/PROXY': {
+        target: 'http://xxxx',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/PROXY/, ''),
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    reportCompressedSize: true,
+    rolldownOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            { name: 'react', test: /node_modules\/react(?:-dom)?/ },
+            { name: 'reactRouter', test: /node_modules\/react-router/ },
+            { name: 'lodashEs', test: /node_modules\/lodash-es/ },
+            { name: 'antd', test: /node_modules\/antd/ },
+            { name: 'ahooks', test: /node_modules\/ahooks/ },
+            { name: 'antdStyle', test: /node_modules\/antd-style/ },
+            { name: 'zustand', test: /node_modules\/zustand/ },
+          ],
         },
-      }),
-      visualizer({ gzipSize: true }),
-      viteCompression({
-        algorithm: 'gzip',
-        verbose: false,
-        filter: /\.(js)$/,
-      }),
-      legacy({
-        targets: ['defaults', 'not IE 11'],
-      }),
-      UnoCSS({
-        configFile: './unocss.config.js',
-      }),
-      vitePluginNoBug(),
-      Printer({
-        info: [
-          ({ lightCyan, green, bold }) => {
-            return `  ${green('➜')}  ${bold('官网')}:  ${lightCyan('https://whyfail.github.io/cwa-docs')}`;
+        minify: {
+          compress: {
+            dropConsole: true,
+            dropDebugger: true,
           },
-        ],
-      }),
-      webUpdateNotice({
-        notificationProps: {
-          title: '系统升级通知',
-          description: '检测到当前系统版本已更新，请刷新页面后使用',
-          buttonText: '刷新',
-          dismissButtonText: '忽略',
-        },
-      }),
-    ],
-    css: {
-      postcss: {
-        plugins: [
-          autoprefixer({
-            grid: true,
-            overrideBrowserslist: ['> 1%'],
-          }),
-        ].filter(Boolean),
-      },
-    },
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-    server: {
-      host: true,
-      open: true,
-      proxy: {
-        // 代理
-        '/PROXY': {
-          target: 'http://xxxx',
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/PROXY/, ''),
         },
       },
     },
-    build: {
-      chunkSizeWarningLimit: 1500,
-      reportCompressedSize: true,
-      rolldownOptions: {
-        output: {
-          advancedChunks: {
-            groups: [
-              { name: 'react', test: /node_modules\/react(?:-dom)?/ },
-              { name: 'reactRouter', test: /node_modules\/react-router/ },
-              { name: 'lodashEs', test: /node_modules\/lodash-es/ },
-              { name: 'antd', test: /node_modules\/antd/ },
-              { name: 'ahooks', test: /node_modules\/ahooks/ },
-              { name: 'antdStyle', test: /node_modules\/antd-style/ },
-              { name: 'zustand', test: /node_modules\/zustand/ },
-            ],
-          },
-          minify: {
-            compress: {
-              dropConsole: true,
-              dropDebugger: true,
-            },
-          },
-        },
-      },
-    },
-  };
-});
+  },
+}));
